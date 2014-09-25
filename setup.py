2f0579545c770569334aed09e718b4630c56ac63
Jeff Klukas <jeff@klukas.net>
2014-09-25 15:53:57 -0500

    Bumping version to 1.0!

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # http://packaging.python.org/en/latest/tutorial.html#version
-    version='0.0.1',
+    version='1.0.0',
 
     description='Creating and rehydrating code skeletons since 2014',
     # long_description=long_description,
@@ -36,7 +36,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
